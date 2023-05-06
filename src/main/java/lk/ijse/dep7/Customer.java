291fbc26e81d3e84b2fdb0ad03db9acd811bb926
dasith <dasiththisaru@gmail.com>
2023-05-06 12:07:36 +0530

    revamp doPost method

--- src/main/java/lk/ijse/dep7/Customer.java
+++ src/main/java/lk/ijse/dep7/Customer.java
@@ -1,2 +1,52 @@
-package lk.ijse.dep7;public class Customer {
+package lk.ijse.dep7;
+
+import java.io.Serializable;
+
+public class Customer implements Serializable {
+
+    private String id;
+    private String name;
+    private String Address;
+
+    public Customer() {
+    }
+
+    public Customer(String id, String name, String address) {
+        this.id = id;
+        this.name = name;
+        Address = address;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", Address='" + Address + '\'' +
+                '}';
+    }
 }
--- src/main/java/lk/ijse/dep7/CustomerServlet.java
+++ src/main/java/lk/ijse/dep7/CustomerServlet.java
@@ -1,2 +1,57 @@
-package lk.ijse.dep7;public class CustomerServlet {
+package lk.ijse.dep7;
+
+import com.sun.jndi.url.corbaname.corbanameURLContextFactory;
+import jakarta.json.Json;
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/customers")
+public class CustomerServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException { .
+
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (!req.getContentType().equals("application/json")){
+            /*TODO: 2023-05-06 send an error massage*/
+            return;
+        }
+        /*BufferedReader out = req.getReader();
+        while ( (line = out.readLine()) != null){
+            stringBuffer.append(line);
+        }*/
+        /*StringBuffer body = new StringBuffer();
+        req.getReader().lines().forEach(line -> body.append(line));
+        System.out.println(body);
+*/
+        Jsonb jsonb = JsonbBuilder.create();
+        Customer customer = jsonb.fromJson(req.getReader(), Customer.class);
+        System.out.println(customer.getId());
+        System.out.println(customer.getName());
+        System.out.println(customer.getAddress());
+
+
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
 }
